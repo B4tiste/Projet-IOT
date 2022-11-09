b084b2a2229d091751ff32e9825bec89df30d873
b4tiste <batiste.laloi@gmail.com>
2022-11-09 11:19:48 +0100

    Ajout du caoteur de temp/humi/pres et de luminosité sur microbit

--- microbit/microbit-samples/source/main.cpp
+++ microbit/microbit-samples/source/main.cpp
@@ -1,40 +1,55 @@
 #include "MicroBit.h"
-
+#include "bme280.h"
+#include "tsl256x.h"
 
 MicroBit uBit;
-MicroBitThermometer termo;
-
-void onButton(MicroBitEvent e)
-{
-    if (e.source == MICROBIT_ID_BUTTON_A)
-        uBit.display.scroll("A");
-        
-
-    if (e.source == MICROBIT_ID_BUTTON_B)
-        uBit.display.scroll("B"); 
-}
+MicroBitI2C i2c(I2C_SDA0, I2C_SCL0);
 
 int main()
 {
     // Initialise the micro:bit runtime.
     uBit.init();
 
-    uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_EVT_ANY, onButton);
-    uBit.messageBus.listen(MICROBIT_ID_BUTTON_B, MICROBIT_EVT_ANY, onButton);
+    // Init liaison i2c capteur Temp/Humi/Press
+    bme280 bme(&uBit, &i2c);
+    uint32_t pressure = 0;
+    int32_t temp = 0;
+    uint16_t humidite = 0;
 
-    // int temp = termo.getTemperature();
-    // if(temp > 30)
-    // {
-    //     ManagedString display = "tro cho:" + ManagedString(temp) + "C";
-    //     uBit.display.scroll(display.toCharArray());
-    // }
-    // else
-    // {
-    //     uBit.display.scroll(temp);
-    // }
+    // Init laison i2c capteur Luminosité
+    tsl256x tsl(&uBit, &i2c);
+    uint16_t comb = 0;
+    uint16_t ir = 0;
+    uint32_t lux = 0;
 
-    // If main exits, there may still be other fibers running or registered event handlers etc.
-    // Simply release this fiber, which will mean we enter the scheduler. Worse case, we then
-    // sit in the idle task forever, in a power efficient sleep.
+    while (true)
+    {
+        // Récupération des données sur les capteurs
+        bme.sensor_read(&pressure, &temp, &humidite);
+        tsl.sensor_read(&comb, &ir, &lux);
+
+        // Temperature
+        int tmp = bme.compensate_temperature(temp);
+        ManagedString display = "T:" + ManagedString(tmp / 100) + "." + (tmp > 0 ? ManagedString(tmp % 100) : ManagedString((-tmp) % 100)) + " C";
+        uBit.display.scroll(display.toCharArray());
+
+        // Humidité
+        // int pres = bme.compensate_pressure(pressure) / 100;
+        // display = "H:" + ManagedString(hum / 100) + "." + ManagedString(tmp % 100) + " rH";
+        // uBit.display.scroll(display.toCharArray());
+
+        // Pression
+        // int hum = bme.compensate_humidity(humidite);
+        // display = "P:" + ManagedString(pres) + " hPa";
+        // uBit.display.scroll(display.toCharArray());
+
+        // Luminosité
+        display = "L:" + ManagedString((int) lux);
+        uBit.display.scroll(display.toCharArray());
+        
+
+        uBit.sleep(1000);
+    }
+
     release_fiber();
 }
