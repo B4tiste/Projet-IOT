f08bf4f71d32665b93f500fe1af60b5318355516
Louisa Chaduc <youreastonefox>
2022-11-09 11:21:05 +0100

    Ecriture sur l'écran oled

--- microbit/microbit-samples/source/main.cpp
+++ microbit/microbit-samples/source/main.cpp
@@ -1,18 +1,22 @@
 #include "MicroBit.h"
+#include "ssd1306.h"
 
 
 MicroBit uBit;
-MicroBitThermometer termo;
+MicroBitI2C i2c(I2C_SDA0,I2C_SCL0);
+MicroBitPin P0(MICROBIT_ID_IO_P0, MICROBIT_PIN_P0, PIN_CAPABILITY_DIGITAL_OUT);
 
 void onButton(MicroBitEvent e)
 {
     if (e.source == MICROBIT_ID_BUTTON_A)
-        uBit.display.scroll("A");
+        uBit.display.scroll("C");
         
 
     if (e.source == MICROBIT_ID_BUTTON_B)
-        uBit.display.scroll("B"); 
+        uBit.display.scroll("D"); 
 }
+
+// Coller le contenu du dossier ssd1306 !
 
 int main()
 {
@@ -20,21 +24,20 @@ int main()
     uBit.init();
 
     uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_EVT_ANY, onButton);
-    uBit.messageBus.listen(MICROBIT_ID_BUTTON_B, MICROBIT_EVT_ANY, onButton);
 
-    // int temp = termo.getTemperature();
-    // if(temp > 30)
-    // {
-    //     ManagedString display = "tro cho:" + ManagedString(temp) + "C";
-    //     uBit.display.scroll(display.toCharArray());
-    // }
-    // else
-    // {
-    //     uBit.display.scroll(temp);
-    // }
+    // Ecriture sur l'écran OLED
+    ssd1306 screen(&uBit, &i2c, &P0);
+    while(true)
+    {
+        screen.display_line(1,0,"Temperature :");
+        screen.display_line(3,6,"29'C");
+        screen.display_line(5,0,"Luminosite :");
+        screen.display_line(7,6,"15%");
+        screen.update_screen();
+        uBit.sleep(1000);
+    }
 
-    // If main exits, there may still be other fibers running or registered event handlers etc.
-    // Simply release this fiber, which will mean we enter the scheduler. Worse case, we then
-    // sit in the idle task forever, in a power efficient sleep.
     release_fiber();
+
 }
+
