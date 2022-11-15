08f2253ff691036914c1e594bc80a5f470315ee7
Mathis Medard <mathismedard.74@gmail.com>
2022-11-15 01:04:36 +0100

    Message Sender and Receiver with Blocking queue (not tested) part 2

---
+++ application/app/src/main/java/com/cpe/irc/projet_iot/communication/Messager.java
@@ -0,0 +1,24 @@
+package com.cpe.irc.projet_iot.communication;
+
+import java.net.DatagramSocket;
+import java.util.concurrent.BlockingQueue;
+
+public abstract class Messager {
+    protected static int lastId = 0;
+    protected BlockingQueue<Message> queue;
+    protected int uniqueId;
+    protected DatagramSocket UDPSocket;
+
+    public static int incrementId() {
+        return Messager.lastId++;
+    }
+
+    protected Messager(BlockingQueue<Message> queue) {
+        this.uniqueId = Receiver.incrementId();
+        this.queue = queue;
+    }
+
+    public Message getMessage() throws InterruptedException {
+        return this.queue.take();
+    }
+}
